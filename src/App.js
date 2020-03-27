cf45afde28c4d9d0cfee6eb84aab44fd8e0801dd
Aman Kubanychbek <amankubanychbek@gmail.com>
2020-03-27 20:36:22 +0600

    add class api usage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function timeLogger() {
+  const date = new Date()
+
+  console.log(date.toLocaleTimeString())
+}
+
+class App extends React.Component {
+  state = {
+    value: 0
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(timeLogger, 1000)
+  }
+
+  componentDidUpdate() {
+    console.log(this.state.value)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId)
+  }
+
+  decrement = () => this.setState(prevState => ({value: prevState.value - 1}))
+
+  increment = () => this.setState(prevState => ({value: prevState.value + 1}))
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Counter: {this.state.value}
+          </p>
+          <button onClick={this.decrement}>decrement</button>
+          <button onClick={this.increment}>increment</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
